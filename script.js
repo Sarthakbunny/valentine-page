b0dcf70781a0ffcd529d98e92720931f90e111ab
Sarthak Srivastava <2016ceb1017@iitrpr.ac.in>
2026-02-11 01:02:16 +0530

    edited bg and script changes

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
 const noBtn = document.querySelector(".no-btn");
 const yesBtn = document.querySelector(".btn[alt='Yes']");
+const headerText = document.getElementById("header-text");
 
 const title = document.getElementById("letter-title");
 const catImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 
-// Click Envelope
+const body = document.querySelector("body");
 
+// Click Envelope
+headerText.style.display = "none";
 envelope.addEventListener("click", () => {
     envelope.style.display = "none";
     letter.style.display = "flex";
 
     setTimeout( () => {
         document.querySelector(".letter-window").classList.add("open");
+        body.style.backgroundImage = 'url("bg2.jpg")';
     },50);
 });
 
@@ -45,6 +49,28 @@ yesBtn.style.transformOrigin = "center center";
 yesBtn.style.transition = "transform 0.3s ease";
 
 noBtn.addEventListener("click", () => {
+
+    switch(yesScale){
+        case 1:
+            headerText.style.display = "block";
+            headerText.style.transform = `scale(1.6)`;
+            break;
+        case 3:
+            headerText.textContent = "Are you sure?";
+            headerText.style.transform = `scale(1.8)`;
+            break;
+        case 5:
+            headerText.textContent = "Are you 100% sure?";
+            headerText.style.transform = `scale(2)`;
+            break;
+        case 7:
+            headerText.textContent = "Maan jaa yr!!";
+            headerText.style.transform = `scale(2.5)`;
+            break;
+        default:
+            break;
+    }
+
     yesScale += 2;
 
     if (yesBtn.style.position !== "fixed") {
@@ -69,4 +95,5 @@ yesBtn.addEventListener("click", () => {
     buttons.style.display = "none";
 
     finalText.style.display = "block";
+    headerText.style.display = "none";
 });
